4462735b46d11a14300b75a1f08bb25798a203c0
matgeomen <matgeomen@gmail.com>
2025-08-30 16:30:28 +0300

    Update Firebase configuration

--- firebase.config.ts
+++ firebase.config.ts
@@ -2,13 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  // Firebase config will be added by user
-  apiKey: "your-api-key",
-  authDomain: "your-project.firebaseapp.com",
-  projectId: "your-project-id",
-  storageBucket: "your-project.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "your-app-id"
+  apiKey: "AIzaSyDJC45bkju3DUARcefHKF7-9e-hUsLRets",
+  authDomain: "takip-sistemi-jlej4.firebaseapp.com",
+  projectId: "takip-sistemi-jlej4",
+  storageBucket: "takip-sistemi-jlej4.firebasestorage.app",
+  messagingSenderId: "250028828318",
+  appId: "1:250028828318:web:965696395b5e9dbadb7d8f"
 };
 
 const app = initializeApp(firebaseConfig);
